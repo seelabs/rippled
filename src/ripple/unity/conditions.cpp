8a76e88c9237af681e2fc94d68733f746bbf9b1f
seelabs <scott.determan@yahoo.com>
2017-07-07 10:50:52 -0400

    ASN.1 decoders and encoders

--- src/ripple/unity/conditions.cpp
+++ src/ripple/unity/conditions.cpp
@@ -19,5 +19,6 @@
 
 
 #include <ripple/conditions/impl/Condition.cpp>
+#include <ripple/conditions/impl/Der.cpp>
 #include <ripple/conditions/impl/Fulfillment.cpp>
 #include <ripple/conditions/impl/error.cpp>
